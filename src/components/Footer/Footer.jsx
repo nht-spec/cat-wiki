aeddb16c8930ad1269b2a5163540ccba4dcbb9ee
Thinh Nguyen <hoangthinh.vn44@gmail.com>
2021-11-13 14:57:20 +0700

    scrooll down to hide footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useScroll } from '../../features/CatWiki/hooks/useScroll';
 import './style.scss';
 Footer.propTypes = {};
 
-function Footer(props) {
+function Footer() {
+	const { scrollDirection } = useScroll();
+	const styles = {
+		active: {
+			transition: 'transform ease-in-out 0.8s',
+		},
+		hidden: {
+			transition: 'transform ease-in-out 0.8s',
+			transform: 'translateY(100%)',
+		},
+	};
+
 	return (
 		<div className='fixed'>
-			<div className='list-content'>
+			<div
+				className='list-content'
+				style={
+					(scrollDirection === undefined && styles.active) ||
+					scrollDirection === 'down'
+						? styles.active
+						: styles.hidden
+				}
+			>
 				<div className='logo'>
 					<h2 className='text-logo'>CatWiki</h2>
 					<div className='image-logo'>
