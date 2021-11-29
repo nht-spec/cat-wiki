ca1158b3855fab457d72d8773b701eb988aad5d7
Thinh Nguyen <hoangthinh.vn44@gmail.com>
2021-11-29 15:45:11 +0700

    fix image benefit error

--- src/features/CatWiki/components/Benefits/Benefits.jsx
+++ src/features/CatWiki/components/Benefits/Benefits.jsx
@@ -22,13 +22,13 @@ function Benefits(props) {
 				<div className='image-cat-one'>
 					<img
 						className='img'
-						src='https://s3-alpha-sig.figma.com/img/aab6/9287/2c4d929be0dbd9efe5e7dabd4bb08ac7?Expires=1637539200&Signature=a8k-3NRs8h9nchElQ8kMSsefu00BqiKDRzOHdyvaTeuGdPJ-8~4LkU~N-hmp3UwTtoyFsMLnRqr9VlUyDaVWAl8QxeM8N5DlQslFj3touj9KvCfJYYhay0lb6iPhg14GnMdc09cPE03G2BHSRoyUCtMwIdXDC-wPH-14pmT4xdCZlN4lxoY63nKmNGcc0exoszazVUAaNqRQNUcAnA8tTOSFTVexRwYeZz6Na07voscTfpJOxehLr725fRP2e~oU6nLFqoDuH1noVsu8dGQJBgdOjU~m9P0-Z-QPX5lVY-TwzCf-v6sAeoggErggALN9PUZ7RvT4A5PkZvEYg4X07w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+						src='https://s3-alpha-sig.figma.com/img/aab6/9287/2c4d929be0dbd9efe5e7dabd4bb08ac7?Expires=1639353600&Signature=GoANQ2B10bv0JOby9nSYN3z4777wno6fPEz-FXkHScIPfLbuwKf7nF-FQJcIoADBK81W2woreV5EP2IneTRZ2NOPcRcSWwBYQoiu1E-HXE5Rmim--cszRMyCsTUZHGGivTK9D0NwC7RpFv34vnNOqkYwf1PjPfzpPNGxVEA067jZHQVA8M3oF688Ffm0M0AlNB8Zwrme~fhhZDSeOhACv4n4BRf4MR5-Fr94MTKelkRYAtdBFdGuL8UPQTzEyp-yh5AYTBmAvY1XHunLxm8MxZj9~dAZhP4D6fK2C6CSTP3kFyWKP2N6RLh1LLWaEWwOZVzw-2TLRC9IPsDlxh1trg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
 						alt=''
 					/>
 					<div className='image-cat-three'>
 						<img
 							className='img'
-							src='https://s3-alpha-sig.figma.com/img/67f4/a6fd/8e772f937cf9ff7fca1e81286456f193?Expires=1637539200&Signature=fDGEftjFcO-sFTpzi1wKIdBu40K78TcdRgzEbyuj7RMCBC~BVSFp2KKsuMAZc9TVvXMaiVPo4aPURzL4sObAhDbKd-m1gpkN21Y50s7Vq1FBXV6ulCvRSJi~RiWLf8KFbIY~VoDPyyyZa6VzPydwCQhtT9KtWuvyeXI7rkFy44lf-Pg~6GmIlhYUGqdwapvgZduyQgzUyIaBhani4ExoeghojR3B5eMrXC-drbJfZgZTU5-xQAPoBTaSOPWcZ6brjma6dsm4ShwwP0iNee8s~sktNwHJOK5XflkAN~oVsVEn~pOwn~ChHf7N9ZYSiJjBlp5k0lTh49R6I30cpOhY-A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+							src='https://s3-alpha-sig.figma.com/img/67f4/a6fd/8e772f937cf9ff7fca1e81286456f193?Expires=1639353600&Signature=KkkMf9KY-pjrEk5Nq9mAiHQ1LaBBg-WXcGnPoqozwb5Qgb949E-ivzk9ZXOSrxaFYwX4HN4JmQSNvNEQuZdPUcM2~OjMblwrM621jAw-MmqU0P8lc5sUM6syQQ1v2HIi6JLEW2ae97zSsHbZU~jKwV-dPEsDYmfa6ifLFtYiX5Lo0hKimZNcgKv2G3u0HkQqh0hznLW9suZHcDPCkLJx-1zuk9DCvzbDE64ECb8sQuxfFv4Jtc42sQimpXB7GwLQ2e6SKJjlR~IzLfAFAxwAsd0UYz9QrXbxgaaSjKfhml-muQfMl5GP-17K~wldJeH4ZQ1IFntfy4oJWe0M8TZKzw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
 							alt=''
 						/>
 					</div>
@@ -36,7 +36,7 @@ function Benefits(props) {
 				<div className='image-cat-two'>
 					<img
 						className='img'
-						src='https://s3-alpha-sig.figma.com/img/dad1/068a/80c9cd0e261de43071ed2be676b7b1cd?Expires=1637539200&Signature=RsOj3T5pxm9-iBlEFk4nU7w9fSCErmZk4kQdP6iWbGYop3l31R1ocklas8soH~g~MyJ3b-kryUZAjnyQzzxGChjauf3ic1CuJyYXj5lIhi0ANXtmr34IJ-hmaDNcyeJPFu-qevYdvXsa2BIVDFXZDDJhr1uGHd-zcE7filJ3hMbqvIdUxplAOmCRQHYO-~QArOFExGIjp1mh88oaaRAi7nyNq-vb5sZEDbSdM9SfKappp7B51UYQHYcWMsHs17h6-FaeDj89wKeOD0xa5U7Ec81dB74uDWFpaf47TcmeE-51pHmTxxK7Tqd5m~owgzFZgqRQzwMNflhWjAyjGOMAPA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+						src='https://s3-alpha-sig.figma.com/img/dad1/068a/80c9cd0e261de43071ed2be676b7b1cd?Expires=1639353600&Signature=aN1m4iou-UiQIKz2XJ5JE0UZR438LDjrD8cVLA5fABbGTfIklr6yMmZq7nYgriX9J3AfC0bG4O-bR-hlDucZtHMtq~VFxDvfblTTilEgVFKg89T3bqgZY4qWM8IK5yxsaVg0vtkSz4OLnZ04wCnm~QRjLSUp5EvLmfOho~7ACupgpnuIf1yKJ0kizxDVCbkvu0eCwf~ysK009QsYYNxqOQXHyGEKbsPShTb3E9TMWyMNtCkRnpE049l24U~2zrWNhoK5yGC2v8vu1AjSMLxiKen5gXPwIUOOkTrI99xQVqVALmJlnK0uDkrxmWWPRQrj3-yozjtli2LHOboFwfFtpg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
 						alt=''
 					/>
 				</div>
